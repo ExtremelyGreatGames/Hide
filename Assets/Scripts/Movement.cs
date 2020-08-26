c4c05fac66d2b55bab99be35f0c25ee89ad9ae7c
Harrold Paderan <harrold_paderan@yahoo.com>
2020-08-25 19:50:26 -0600

    simple transformation

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Update()
+    {
+        float hori = Input.GetAxisRaw("Horizontal");
+        float verti = Input.GetAxisRaw("Vertical");
+
+        if (hori != 0) {
+
+        }
+
+        if (verti != 0) {
+            
+        }
+    }
+}
