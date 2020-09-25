68dc97eab45c68e1518d4c87c09a1533b4d22d6a
Harrold Paderan <harrold@ualberta.ca>
2020-09-24 19:45:04 -0600

    multiplayer with movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    public float moveSpeed = 5.0f;
+
+    Inputs controls;
+    Vector2 _move;
+
+    Vector2 test_move;
+
+    void Awake()
+    {
+        /*****************************************************/
+        /* Need to find a way to switch between hider/seeker */
+        /*****************************************************/
+
+        controls = new Inputs();
+
+        controls.Hider.Movement.performed += ctx => _move = ctx.ReadValue<Vector2>();
+        controls.Hider.Movement.canceled += ctx => _move = Vector2.zero;
+    }
+    
+    void OnEnable()
+    {
+        controls.Hider.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.Hider.Disable();
+    }
+
+    void Update() {
+        if (!isLocalPlayer) return;
+
+        transform.Translate(_move * Time.deltaTime * moveSpeed);
+    }
+}
